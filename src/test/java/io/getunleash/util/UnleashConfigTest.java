53c8c49f6d9e0bc9f6782f94dc57321ae0d46906
Gastón Fournier <gaston@getunleash.io>
2025-08-07 17:20:06 +0200

    chore: change registration name to unleash-java-sdk

--- src/test/java/io/getunleash/util/UnleashConfigTest.java
+++ src/test/java/io/getunleash/util/UnleashConfigTest.java
@@ -113,7 +113,7 @@ public class UnleashConfigTest {
         UnleashConfig config =
                 UnleashConfig.builder().appName("my-app").unleashAPI("http://unleash.org").build();
 
-        assertThat(config.getSdkVersion()).isEqualTo("unleash-client-java:development");
+        assertThat(config.getSdkVersion()).isEqualTo("unleash-java-sdk:development");
     }
 
     @Test
@@ -159,7 +159,7 @@ public class UnleashConfigTest {
         assertThat(connection.getRequestProperty(UNLEASH_CONNECTION_ID_HEADER))
                 .isEqualTo(unleashConfig.getConnectionId());
         assertThat(connection.getRequestProperty(UNLEASH_SDK_HEADER))
-                .isEqualTo("unleash-client-java:development");
+                .isEqualTo("unleash-java-sdk:development");
         assertThat(connection.getRequestProperty("User-Agent")).isEqualTo(appName);
     }
 
