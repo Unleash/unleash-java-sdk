b420c0ee6aee3f9f2f48a87842ba8386df4e883b
Christopher Kolstad <chriswk@getunleash.io>
2025-10-27 10:39:31 +0100

    chore(examples): Added example for native-image

---
+++ examples/native-image-example/src/main/java/io/getunleash/example/Constraints.java
@@ -0,0 +1,53 @@
+package io.getunleash.example;
+
+import io.getunleash.DefaultUnleash;
+import io.getunleash.Unleash;
+import io.getunleash.UnleashContext;
+import io.getunleash.util.UnleashConfig;
+import io.getunleash.event.UnleashReady;
+import io.getunleash.event.ClientFeaturesResponse;
+import io.getunleash.event.UnleashSubscriber;
+
+public class Constraints {
+
+    public static void main(String[] args) throws InterruptedException {
+        UnleashConfig config = UnleashConfig.builder()
+                .appName("client-example.advanced.java")
+                .customHttpHeader(
+                        "Authorization",
+                        getOrElse("UNLEASH_API_TOKEN",
+                                "*:development.25a06b75248528f8ca93ce179dcdd141aedfb632231e0d21fd8ff349"))
+                .unleashAPI(getOrElse("UNLEASH_API_URL", "https://app.unleash-hosted.com/demo/api"))
+                .instanceId("java-example")
+                .synchronousFetchOnInitialisation(true)
+                .subscriber(new UnleashSubscriber() {
+                        public void onReady(UnleashReady ready) {
+                            System.out.println("Unleash is ready");
+                        }
+                        public void togglesFetched(ClientFeaturesResponse toggleResponse) {
+                            System.out.println("Fetch toggles with status: " + toggleResponse.getStatus());
+                        }
+                    })
+                .sendMetricsInterval(30).build();
+
+        Unleash unleash = new DefaultUnleash(config);
+        UnleashContext context = UnleashContext.builder()
+            .addProperty("semver", "1.5.2")
+            .build();
+        UnleashContext smallerSemver = UnleashContext.builder()
+            .addProperty("semver", "1.1.0")
+            .build();
+        while (true) {
+                    unleash.isEnabled("advanced.constraints", context); // expect this to be true
+                    unleash.isEnabled("advanced.constraints", smallerSemver); // expect this to be false
+        }
+    }
+
+    public static String getOrElse(String key, String defaultValue) {
+        String value = System.getenv(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
+}
