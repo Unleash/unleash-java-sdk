62ab17269285ea66db083acb1214bfcfa0faabe9
Christopher Kolstad <chriswk@getunleash.io>
2025-11-11 10:24:04 +0100

    updated cli-example to have configurable interval

--- examples/cli-example/build.gradle.kts
+++ examples/cli-example/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     application
+    id("com.google.cloud.tools.jib").version("3.4.5")
 }
 
 application {
@@ -12,7 +13,19 @@ repositories {
     mavenLocal()
 }
 
+java {
+    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+jib {
+    container {
+        mainClass = "io.getunleash.example.AdvancedConstraints"
+    }
+}
+
 dependencies {
     implementation("io.getunleash:unleash-client-java:11.2.0-SNAPSHOT")
+    implementation("org.slf4j:slf4j-api:2.0.17")
     implementation("ch.qos.logback:logback-classic:1.5.20")
 }
